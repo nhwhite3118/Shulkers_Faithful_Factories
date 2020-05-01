c5df5bda2b5ebd14639ee811aa3c704912bb70b3
Nick White <nhwhite3118@gmail.com>
2020-04-30 23:11:29 -0500

    Sapling death chance now depends on light level and scales from 0 at 10 to 0.5 at 15. Broke out random chance threshold variables so config is easier

--- src/main/java/com/nhwhite3118/cobbler/ForgeEventSubscriber.java
+++ src/main/java/com/nhwhite3118/cobbler/ForgeEventSubscriber.java
@@ -4,12 +4,10 @@ import java.util.Random;
 
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
-import net.minecraft.client.Minecraft;
 import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.biome.Biome;
-import net.minecraft.world.chunk.Chunk;
 import net.minecraftforge.common.IPlantable;
 import net.minecraftforge.event.entity.player.BonemealEvent;
 import net.minecraftforge.event.world.SaplingGrowTreeEvent;
@@ -19,11 +17,13 @@ import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 
 @EventBusSubscriber(modid=Cobbler.MODID, bus = EventBusSubscriber.Bus.FORGE)
 public final class ForgeEventSubscriber {
+	private static final double BONEMEAL_SAPLING_ATTEMPT_PERCENT = 0.15;
+	private static final double SAPLING_MAX_DRY_PERCENT = 0.5;
 
 	@SubscribeEvent
     public static void onUseBonemeal(BonemealEvent event) {
 		Random rand = new Random();
-		if(rand.nextFloat() > 0.15) {
+		if(rand.nextFloat() > BONEMEAL_SAPLING_ATTEMPT_PERCENT) {
 			return;
 		}
 		World world = event.getWorld();
@@ -63,9 +63,10 @@ public final class ForgeEventSubscriber {
 		
 		Random rand = new Random();
 		BlockPos saplingPos = event.getPos();
+		double saplingDryPercent = SAPLING_MAX_DRY_PERCENT * (((double)(world.getLight(saplingPos) - 10)) / 5.0);
 		
 		// func_226691_t_ appears to get the biome of a given BlockPos
-		if(rand.nextFloat() <= 0.3 && world.func_226691_t_(saplingPos).getCategory() == Biome.Category.DESERT) {
+		if(rand.nextFloat() <= saplingDryPercent && world.func_226691_t_(saplingPos).getCategory() == Biome.Category.DESERT) {
 			world.setBlockState(saplingPos, Blocks.DEAD_BUSH.getDefaultState());
 			event.setResult(Result.DENY);
 		}
