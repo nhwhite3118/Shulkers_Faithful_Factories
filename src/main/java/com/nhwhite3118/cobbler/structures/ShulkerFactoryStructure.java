4ac616c9be845253aaf8f62e1b43b4420f906c3f
Nick White <nhwhite3118@gmail.com>
2020-08-06 04:40:10 -0500

    locate optimization for shulker factories. Only seems to have a minor improvement for endcity chests, but it's much faster than before if you /locate at 0,0

--- src/main/java/com/nhwhite3118/cobbler/structures/ShulkerFactoryStructure.java
+++ src/main/java/com/nhwhite3118/cobbler/structures/ShulkerFactoryStructure.java
@@ -6,16 +6,24 @@ import com.mojang.serialization.Codec;
 import com.nhwhite3118.cobbler.Cobbler;
 
 import net.minecraft.util.Rotation;
+import net.minecraft.util.SharedSeedRandom;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.ChunkPos;
 import net.minecraft.util.math.MutableBoundingBox;
+import net.minecraft.util.math.SectionPos;
+import net.minecraft.world.IWorldReader;
 import net.minecraft.world.biome.Biome;
+import net.minecraft.world.chunk.ChunkStatus;
+import net.minecraft.world.chunk.IChunk;
 import net.minecraft.world.gen.ChunkGenerator;
 import net.minecraft.world.gen.Heightmap;
 import net.minecraft.world.gen.feature.IFeatureConfig;
 import net.minecraft.world.gen.feature.NoFeatureConfig;
 import net.minecraft.world.gen.feature.structure.Structure;
+import net.minecraft.world.gen.feature.structure.StructureManager;
 import net.minecraft.world.gen.feature.structure.StructureStart;
 import net.minecraft.world.gen.feature.template.TemplateManager;
+import net.minecraft.world.gen.settings.StructureSeparationSettings;
 
 public class ShulkerFactoryStructure extends Structure<NoFeatureConfig> {
 
@@ -50,6 +58,53 @@ public class ShulkerFactoryStructure extends Structure<NoFeatureConfig> {
      */
     protected int getSeedModifier() {
         return 261892189;
+    }
+
+    @Override
+    public BlockPos func_236388_a_(IWorldReader worldView, StructureManager structureAccessor, BlockPos blockPos, int radius, boolean skipExistingChunks,
+            long seed, StructureSeparationSettings structureConfig) {
+        return locateStructureFast(worldView, structureAccessor, blockPos, radius, skipExistingChunks, seed, structureConfig, this);
+    }
+
+    public static BlockPos locateStructureFast(IWorldReader worldView, StructureManager structureAccessor, BlockPos blockPos, int radius,
+            boolean skipExistingChunks, long seed, StructureSeparationSettings structureConfig, Structure<NoFeatureConfig> structure) {
+        int spacing = structureConfig.func_236668_a_();
+        int chunkX = blockPos.getX() >> 4;
+        int chunkZ = blockPos.getZ() >> 4;
+        int currentRadius = 0;
+
+        for (SharedSeedRandom chunkRandom = new SharedSeedRandom(); currentRadius <= 100000; ++currentRadius) {
+            for (int xRadius = -currentRadius; xRadius <= currentRadius; ++xRadius) {
+                boolean xEdge = xRadius == -currentRadius || xRadius == currentRadius;
+
+                for (int zRadius = -currentRadius; zRadius <= currentRadius; ++zRadius) {
+                    boolean zEdge = zRadius == -currentRadius || zRadius == currentRadius;
+                    if (xEdge || zEdge) {
+                        int trueChunkX = chunkX + spacing * xRadius;
+                        int trueChunkZ = chunkZ + spacing * zRadius;
+                        ChunkPos chunkPos = structure.func_236392_a_(structureConfig, seed, chunkRandom, trueChunkX, trueChunkZ);
+                        if (worldView.getNoiseBiome((chunkPos.x << 2) + 2, 60, (chunkPos.z << 2) + 2).hasStructure(structure)) {
+                            IChunk chunk = worldView.getChunk(chunkPos.x, chunkPos.z, ChunkStatus.STRUCTURE_STARTS);
+                            StructureStart<?> structureStart = structureAccessor.func_235013_a_(SectionPos.from(chunk.getPos(), 0), structure, chunk);
+                            if (structureStart != null && structureStart.isValid()) {
+                                if (skipExistingChunks && structureStart.isRefCountBelowMax()) {
+                                    structureStart.incrementRefCount();
+                                    return structureStart.getPos();
+                                }
+
+                                if (!skipExistingChunks) {
+                                    return structureStart.getPos();
+                                }
+                            }
+                        }
+                    } else {
+                        zRadius = currentRadius - 1;
+                    }
+                }
+            }
+        }
+
+        return null;
     }
 
 //    /*
